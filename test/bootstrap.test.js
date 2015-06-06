a2e691d88d6b1637f838ce9de8e6bfd7546671db
Travis Webb <me@traviswebb.com>
2015-06-05 20:20:17 -0400

    call teardown in after()

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -16,48 +16,51 @@ const connections = {
   }
 }
 
-  before(function (done) {
-    let waterline = new Waterline();
+before((done) => {
+  let waterline = new Waterline();
 
-    waterline.loadCollection(Waterline.Collection.extend({
-      identity: 'message',
-      tableName: 'message',
-      connection: [ 'rabbit', 'disk' ],
-      attributes: {
-        title: 'string',
-        content: 'string',
-        stream: {
-          model: 'stream'
-        }
-
-      },
-      routingKey (values) {
-        return values.stream
+  waterline.loadCollection(Waterline.Collection.extend({
+    identity: 'message',
+    tableName: 'message',
+    connection: [ 'rabbit', 'disk' ],
+    attributes: {
+      title: 'string',
+      content: 'string',
+      stream: {
+        model: 'stream'
       }
-    }))
-    waterline.loadCollection(Waterline.Collection.extend({
-      identity: 'stream',
-      tableName: 'stream',
-      connection: [ 'disk' ],
-      attributes: {
-        name: 'string',
-        messages: {
-          collection: 'message',
-          via: 'stream'
-        }
+
+    },
+    routingKey (values) {
+      return values.stream
+    }
+  }))
+  waterline.loadCollection(Waterline.Collection.extend({
+    identity: 'stream',
+    tableName: 'stream',
+    connection: [ 'disk' ],
+    attributes: {
+      name: 'string',
+      messages: {
+        collection: 'message',
+        via: 'stream'
       }
-    }))
+    }
+  }))
 
-    waterline.initialize({
-        adapters: adapters,
-        connections: connections
-      },
-      function (err, ontology) {
-        if (err) return done(err);
+  waterline.initialize({
+      adapters: adapters,
+      connections: connections
+    },
+    function (err, ontology) {
+      if (err) return done(err);
 
-        global.models = ontology.collections
-        done(err);
-      }
-    )
-  })
+      global.models = ontology.collections
+      done(err);
+    }
+  )
+})
 
+after((done) => {
+  Adapter.teardown(done)
+})
